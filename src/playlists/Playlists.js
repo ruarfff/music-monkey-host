0759cc893cd4fa69ddc72cc18a0ef112f878e9c6
Ruairi O Brien <ruairitobrien@gmail.com>
2018-04-22 16:12:59 +0100

    Added error handling for playlists

--- src/playlists/Playlists.js
+++ src/playlists/Playlists.js
@@ -18,20 +18,28 @@ const Playlists = ({ playlists }) => (
   <div style={styles.root}>
     <GridList cellHeight={180} style={styles.gridList}>
       <Subheader>Your Playlists</Subheader>
-      {playlists.map(playlist => (
-        <a href={playlist.external_urls.spotify} target="_blank" key={playlist.id}>
-          <GridTile            
-            title={playlist.name}
-            subtitle={
-              <span>
-                <b>{playlist.tracks.total}</b> tracks
-              </span>
-            }
+      {playlists.length === 0 ? (
+        <p>No Playlists</p>
+      ) : (
+        playlists.map(playlist => (
+          <a
+            href={playlist.external_urls.spotify}
+            target="_blank"
+            key={playlist.id}
           >
-            <img src={playlist.images[0].url} alt="playlist" />
-          </GridTile>
-        </a>
-      ))}
+            <GridTile
+              title={playlist.name}
+              subtitle={
+                <span>
+                  <b>{playlist.tracks.total}</b> tracks
+                </span>
+              }
+            >
+              <img src={playlist.images.length > 0 ? playlist.images[0].url : '/img/partycover.png'} alt="playlist" />
+            </GridTile>
+          </a>
+        ))
+      )}
     </GridList>
   </div>
 );
