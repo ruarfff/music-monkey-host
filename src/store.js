ec53499a5e5ff5207268cdc3e59b69aefa88bc9f
Ruairi O Brien <ruairitobrien@gmail.com>
2018-04-23 15:36:02 +0100

    Added redux logger

--- src/store.js
+++ src/store.js
@@ -1,10 +1,14 @@
 import { applyMiddleware, compose, createStore } from 'redux'
+import { createLogger } from 'redux-logger'
 import createSagaMiddleware from 'redux-saga'
 import { routerMiddleware } from 'react-router-redux'
 import createHistory from 'history/createBrowserHistory'
 import reduxImmutableStateInvariant from 'redux-immutable-state-invariant'
+import LogRocket from 'logrocket'
 import reducers from './reducers'
 import saga from './saga'
+
+LogRocket.init('ftequc/music-monkey-host')
 
 const isDevBuild = process.env.NODE_ENV !== 'production'
 const history = createHistory()
@@ -12,7 +16,17 @@ const history = createHistory()
 const reactRouterMiddleware = routerMiddleware(history)
 const sagaMiddleware = createSagaMiddleware()
 
-const rootMidlelwares = [reactRouterMiddleware, sagaMiddleware]
+const logger = createLogger({
+  duration: true
+})
+
+const rootMidlelwares = [
+  reactRouterMiddleware,
+  sagaMiddleware,
+  logger,
+  LogRocket.reduxMiddleware()
+]
+
 const middleware = isDevBuild
   ? [...rootMidlelwares, reduxImmutableStateInvariant()]
   : rootMidlelwares
