cab1777102be41175a2f53a1b812bfe81829a30b
Ruairi O Brien <ruairitobrien@gmail.com>
2018-04-28 23:48:14 +0100

    Removed some placeholder code from creat playlist dialog.

--- src/events/CreatePlaylistDialog.js
+++ src/events/CreatePlaylistDialog.js
@@ -1,31 +1,22 @@
 import React from 'react'
 import PropTypes from 'prop-types'
 import Button from 'material-ui/Button'
-import TextField from 'material-ui/TextField'
 import Dialog, {
   DialogActions,
   DialogContent,
-  DialogContentText,
   DialogTitle
 } from 'material-ui/Dialog'
 
 const CreatePlaylistDialog = ({ open, onClose }) => (
-  <Dialog open={open} onClose={onClose} aria-labelledby="form-dialog-title">
-    <DialogTitle id="form-dialog-title">Subscribe</DialogTitle>
-    <DialogContent>
-      <DialogContentText>
-        To subscribe to this website, please enter your email address here. We
-        will send updates occationally.
-      </DialogContentText>
-      <TextField
-        autoFocus
-        margin="dense"
-        id="name"
-        label="Email Address"
-        type="email"
-        fullWidth
-      />
-    </DialogContent>
+  <Dialog
+    open={open}
+    onClose={onClose}
+    aria-labelledby="create-playlist-form-dialog-title"
+  >
+    <DialogTitle id="create-playlist-form-dialog-title">
+      Create Playlist
+    </DialogTitle>
+    <DialogContent />
     <DialogActions>
       <Button onClick={onClose} color="primary">
         Cancel
