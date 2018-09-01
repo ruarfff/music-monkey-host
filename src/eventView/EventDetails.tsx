b86fa35d08e0e25218797a52bfcc372b8f0a959b
Ruairi O Brien <ruairitobrien@gmail.com>
2018-09-01 13:56:25 +0100

    Styled event summary view a bit

--- src/eventView/EventDetails.tsx
+++ src/eventView/EventDetails.tsx
@@ -1,3 +1,4 @@
+import { Grid } from '@material-ui/core'
 import Paper from '@material-ui/core/Paper/Paper'
 import Typography from '@material-ui/core/Typography/Typography'
 import EditIcon from '@material-ui/icons/Edit'
@@ -14,52 +15,73 @@ const EventDetails: React.SFC<IEventDetailsProps> = ({
   event
 }: IEventDetailsProps) => (
   <Paper className="EventDetails-root">
-    <Typography variant="display3" gutterBottom={true}>
-      {event.name}
-    </Typography>
+    <Grid className="EventDetails-grid" container={true} spacing={8}>
+      <Grid item={true} xs={12}>
+        <Typography
+          className="EventDetails-title"
+          variant="headline"
+          gutterBottom={true}
+        >
+          Event Details
+        </Typography>
+      </Grid>
+      <Grid className="EventDetails-title-container" item={true} xs={6}>
+        <img
+          className="EventDetails-img"
+          src={event.imageUrl}
+          alt={event.name}
+        />
+      </Grid>
 
-    <Typography variant="headline" gutterBottom={true}>
-      Organizer: {event.organizer}
-    </Typography>
+      <Grid className="EventDetails-info" item={true} xs={6}>
+        <Typography variant="headline" gutterBottom={true}>
+          {event.name}
+        </Typography>
 
-    <Typography variant="headline" gutterBottom={true}>
-      Description: {event.description}
-    </Typography>
+        <Typography variant="caption" gutterBottom={true}>
+          Starting at:{' '}
+          {event.startDateTime.format('dddd, MMMM Do YYYY, h:mm:ss a')}
+        </Typography>
 
-    <Typography variant="caption" gutterBottom={true}>
-      Starting at: {event.startDateTime.format('dddd, MMMM Do YYYY, h:mm:ss a')}
-    </Typography>
+        {event.eventCode && (
+          <Typography variant="body1" gutterBottom={true}>
+            Event Code: {event.eventCode}
+          </Typography>
+        )}
 
-    <Typography variant="caption" gutterBottom={true}>
-      Ending at: {event.endDateTime.format('dddd, MMMM Do YYYY, h:mm:ss a')}
-    </Typography>
+        {event.location && (
+          <Typography variant="body1" gutterBottom={true}>
+            Location: {event.location.address}
+          </Typography>
+        )}
 
-    {event.eventCode && (
-      <Typography variant="body1" gutterBottom={true}>
-        Event Code: {event.eventCode}
-      </Typography>
-    )}
+        {event.venue && (
+          <Typography variant="body1" gutterBottom={true}>
+            Venue: {event.venue}
+          </Typography>
+        )}
+      </Grid>
 
-    {event.location && (
-      <Typography variant="body1" gutterBottom={true}>
-        Location: {event.location.address}
-      </Typography>
-    )}
-
-    {event.venue && (
-      <Typography variant="body1" gutterBottom={true}>
-        Venue: {event.venue}
-      </Typography>
-    )}
-
-    <LinkButton
-      variant="raised"
-      color="primary"
-      to={location.pathname + '/edit'}
-    >
-      Edit
-      <EditIcon />
-    </LinkButton>
+      <Grid
+        className="EventDetails-actions"
+        justify="flex-end"
+        container={true}
+        item={true}
+        spacing={16}
+        xs={12}
+      >
+        <Grid item={true}>
+          <LinkButton
+            variant="raised"
+            color="primary"
+            to={location.pathname + '/edit'}
+          >
+            Edit Event
+            <EditIcon />
+          </LinkButton>
+        </Grid>
+      </Grid>
+    </Grid>
   </Paper>
 )
 
