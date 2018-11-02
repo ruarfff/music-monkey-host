6b02eec4726ac1f51336cbc1d61f304242db8ce3
Ruairi O Brien <ruairitobrien@gmail.com>
2018-11-02 19:29:57 +0000

    Updated spotify playlist client to latest with typescript

--- src/playlist/playlistClient.ts
+++ src/playlist/playlistClient.ts
@@ -1,14 +1,23 @@
-import * as SpotifyWebApi from 'spotify-web-api-js'
+import SpotifyWebApi from 'spotify-web-api-js'
 import { accessTokenKey } from '../auth/authConstants'
-import localStorage from '../storage/localStorage'
 import http from '../http'
+import localStorage from '../storage/localStorage'
+import IUser from '../user/IUser'
+import IPlaylist from './IPlaylist'
 
-export const addTracksToPlaylist = (playlistId, trackUris) => {
+export const addTracksToPlaylist = (
+  playlistId: string,
+  trackUris: string[]
+) => {
   const spotifyApi = getSpotifyApi()
   return spotifyApi.addTracksToPlaylist(playlistId, trackUris)
 }
 
-export const reOrderPlaylist = (playlist, fromIndex, toIndex) => {
+export const reOrderPlaylist = (
+  playlist: IPlaylist,
+  fromIndex: number,
+  toIndex: number
+) => {
   const spotifyApi = getSpotifyApi()
   let insertBefore = toIndex
   if (fromIndex < toIndex) {
@@ -21,39 +30,46 @@ export const reOrderPlaylist = (playlist, fromIndex, toIndex) => {
   )
 }
 
-export const fetchPlaylist = playlistId => {
+export const fetchPlaylist = (playlistId: string) => {
   const spotifyApi = getSpotifyApi()
   return spotifyApi.getPlaylist(playlistId)
 }
 
-export const fetchUsersPlaylists = async user => {
+export const fetchUsersPlaylists = async (user: IUser) => {
   const res = await http.get('/users/' + user.userId + '/playlists', {
     withCredentials: true
   })
   return res.data
 }
 
-export const createPlaylist = (userId, name, description) => {
+export const createPlaylist = (name: string, description = '') => {
   const spotifyApi = getSpotifyApi()
 
-  return spotifyApi.createPlaylist(userId, {
+  return spotifyApi.createPlaylist({
     description,
     name,
     public: true
   })
 }
 
-export const replaceTracksInPlaylist = (playlistId, trackUris) => {
+export const replaceTracksInPlaylist = (
+  playlistId: string,
+  trackUris: string[]
+) => {
   const spotifyApi = getSpotifyApi()
   return spotifyApi.replaceTracksInPlaylist(playlistId, trackUris)
 }
 
-export const removeTrackFromPlaylist = (playlistId, uri, positions) => {
+export const removeTrackFromPlaylist = (
+  playlistId: string,
+  uri: string,
+  position: number
+) => {
   const spotifyApi = getSpotifyApi()
-  return spotifyApi.removeTracksFromPlaylist(playlistId, [{uri}, positions])
+  return spotifyApi.removeTracksFromPlaylist(playlistId, [{ uri }, position])
 }
 
-export const searchForTracks = async (searchTerm) => {
+export const searchForTracks = async (searchTerm: string) => {
   const response = await http.get(
     '/search?q=' + encodeURIComponent(searchTerm) + '&type=track',
     {
@@ -63,7 +79,7 @@ export const searchForTracks = async (searchTerm) => {
   return response.data
 }
 
-export const getTracksFeatures = (trackIds) => {
+export const getTracksFeatures = (trackIds: string[]) => {
   const spotifyApi = getSpotifyApi()
   return spotifyApi.getAudioFeaturesForTracks(trackIds)
 }
