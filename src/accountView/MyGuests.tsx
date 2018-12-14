72c3debf5a127bdbc1150fa18ee8a9ea11889398
Balyko Eugene <balyko.peaceit@gmail.com>
2018-12-14 17:00:20 +0200

    guests

--- src/accountView/MyGuests.tsx
+++ src/accountView/MyGuests.tsx
@@ -1,20 +1,53 @@
 import * as React from 'react'
+import { uniqBy, flattenDeep } from 'lodash'
 import IEvent from '../event/IEvent'
-// import EventCard from '../event/EventCard'
+import Avatar from '@material-ui/core/Avatar/Avatar'
+import { AccountCircle } from '@material-ui/icons'
+import IEventGuest from '../event/IEventGuest'
 
 interface IMyPlaylistsProps {
   events: IEvent[]
 }
 
 class MyPlaylists extends React.PureComponent<IMyPlaylistsProps> {
+
+  public renderGuests = (guest: IEventGuest, key: number) => {
+    return (
+      <div key={key} className='guestWrapper'>
+        {guest.user.image &&
+          <Avatar
+            alt={guest.user.displayName}
+            src={guest.user.image}
+          />
+        }
+        {!guest.user.image && <AccountCircle />}
+        <div>
+          {guest.user.displayName}
+        </div>
+      </div>
+    )
+  }
+
   public render() {
+    const { events } = this.props
+    const guests = uniqBy(flattenDeep(events.map((event) => event.guests)), 'userId')
+
     return (
       <>
-        {/*{*/}
-          {/*this.props.events.map((event, key) =>*/}
-            {/*<EventCard event={event} key={key}/>*/}
-          {/*)*/}
-        }
+        <div className='guestsContainer'>
+          {
+            guests.length > 0 ? guests.map((guest, key) => {
+              if (guest) {
+                return this.renderGuests(guest, key)
+              } else {
+                return
+              }
+            }) :
+            <div>
+              You don't have any guests
+            </div>
+          }
+        </div>
       </>
     )
   }
